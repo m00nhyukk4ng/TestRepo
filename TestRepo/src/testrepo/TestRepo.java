5bab2adf950fc8a2446d0b036c66cc790ae13209
Moon Hyuk Kang <kangmo@sheridancollege.ca>
2020-06-23 09:22:53 -0400

    10 min second commit

---
+++ TestRepo/src/testrepo/TestRepo.java
@@ -0,0 +1,17 @@
+package testrepo;
+
+/**
+ * This class +++Insert Description Here+++
+ *
+ * @author Moon Hyuk Kang
+ */
+public class TestRepo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+
+}
